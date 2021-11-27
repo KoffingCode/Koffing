be1cbe6a2808a5dee21147c4b4f7d2423008e8b7
WitchImage <simonfelipemunozosorio@gmail.com>
2021-11-27 08:49:13 -0500

    creación de endpoints para parametrizar meseros

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\WaiterController;
 
+Route::apiResource('/waiter', WaiterController::class);
 
 Route::apiResource('/usuario',UsuarioController::class);
 
