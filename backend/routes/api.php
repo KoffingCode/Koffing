4fe7880beffecf4728b924d9a54018ec8e848e89
WitchImage <simonfelipemunozosorio@gmail.com>
2021-11-27 09:11:59 -0500

    actualización de los endpoints de la api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,11 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TableController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\WaiterController;
 
-Route::apiResource('/waiter', WaiterController::class);
+Route::apiResource('/table',TableController::class);
 
 Route::apiResource('/usuario',UsuarioController::class);
 
-Route::apiResource('/table',TableController::class);
+Route::apiResource('/waiter', WaiterController::class);
