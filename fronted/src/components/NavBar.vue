458a97342f5a2a49c98fbabfe003d4b0aec5fc90
DavidCDCB <cristian-cb1@hotmail.com>
2021-12-12 08:23:41 -0500

    iconos en Navbar

--- fronted/src/components/NavBar.vue
+++ fronted/src/components/NavBar.vue
@@ -1,10 +1,11 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-	<router-link class="navbar-brand" to="/">Restaurante Koffing</router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+	<div class="container-fluid">
+		<i class="fas fa-mug-hot mx-1"></i>
+		<router-link class="navbar-brand" to="/">Restaurante Koffing</router-link>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 			<li class="nav-item" v-for="(item,ind) of routes" :key="ind">
@@ -12,10 +13,10 @@
 			</li>
 		</ul>
 		<span class="navbar-text">
-			<router-link class="nav-link" to="/acerca">Acerca del proyecto</router-link>
+			<router-link class="nav-link" to="/acerca"><i class="fas fa-laptop-code mx-1"></i>Acerca del proyecto</router-link>
 		</span>
+		</div>
 	</div>
-  </div>
 </nav>
 </template>
 
